52570dad115863a5bd9c7b0611e1338d8f3ee953
AstridRamadhani-TeknikInformatika <12150121537@students.uin-suska.ac.id>
2023-06-26 22:18:13 +0700

    frontend-buku

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <!--banner image-->
+
+  <div class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center">
+    <div class="content text-center">
+      <h1 class="text-white">WELCOME TO COMICS STORE!</h1>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+.banner-image{
+  background: #8FBC8F;
+  font-family: "Times New Roman" ;
+  font-style: italic;
+  font-size: 30px;
+
+}
+
+</style>
