71615560993d403a64b39aa0a148844b267c1f69
AstridRamadhani-TeknikInformatika <12150121537@students.uin-suska.ac.id>
2023-07-03 20:08:47 +0700

    frontend-buku

--- src/views/Peminjaman.vue
+++ src/views/Peminjaman.vue
@@ -1,5 +1,46 @@
 <template>
   <div>
+    <div class="text-center">
+    <h3>Form Peminjaman Buku</h3>
+    <form @submit.prevent="submitPeminjaman">
+      <div class="form-group">
+        <label for="idPeminjaman">ID Peminjaman</label>
+        <input type="text" class="form-control" v-model="pengembalian.idPeminjaman" id="idPeminjaman" required>
+      </div>
+      <div class="form-group">
+        <label for="tanggal">Tanggal Peminjaman</label>
+        <input type="date" class="form-control" v-model="peminjaman.tanggal" id="tanggal" required>
+      </div>
+      <div class="form-group">
+        <label for="tanggal">Tanggal Pengembalian</label>
+        <input type="date" class="form-control" v-model="pengembalian.tanggal" id="tanggal" required>
+      </div>
+
+      <div class="form-group">
+        <label for="nomorAnggota">Nomor Anggota</label>
+        <input type="text" class="form-control" v-model="peminjaman.nomorAnggota" id="nomorAnggota" required>
+      </div>
+      <div class="form-group">
+        <label for="kodeBuku">Kode Buku</label>
+        <input type="text" class="form-control" v-model="peminjaman.kodeBuku" id="kodeBuku" required>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    <h3>Form Pengembalian Buku</h3>
+    <form @submit.prevent="submitPengembalian">
+      <div class="form-group">
+        <label for="idPeminjaman">ID Peminjaman</label>
+        <input type="text" class="form-control" v-model="pengembalian.idPeminjaman" id="idPeminjaman" required>
+      </div>
+      <div class="form-group">
+        <label for="tanggal">Tanggal Pengembalian</label>
+        <input type="date" class="form-control" v-model="pengembalian.tanggal" id="tanggal" required>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    </div>
+    <input type="number" v-model="selectedPeminjaman" />
+    <button @click="selectPeminjaman" >Pilih Peminjaman</button>
     <h1>List Peminjaman DIPINJAM</h1>
     <table class="table">
       <thead>
@@ -57,6 +98,15 @@ export default {
   name: 'App',
   data() {
     return {
+      peminjaman: {
+        tanggal: '',
+        nomorAnggota: '',
+        kodeBuku: '',
+      },
+      pengembalian: {
+        idPeminjaman: '',
+        tanggalPengembalian: '',
+      },
       peminjamanDipinjam: [],
       peminjamanDikembalikan: [],
     };
@@ -64,14 +114,63 @@ export default {
   mounted() {
     // Mengambil data peminjaman dengan status DIPINJAM dari API backend PHP
     this.fetchPeminjamanDipinjam();
-
     // Mengambil data peminjaman dengan status DIKEMBALIKAN dari API backend PHP
     this.fetchPeminjamanDikembalikan();
+    this.selectPeminjaman();
   },
+
   methods: {
+    submitPeminjaman() {
+      fetch('http://localhost/crud/peminjamanbuku.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(this.peminjaman),
+      })
+          .then(response => response.json())
+          .then(data => {
+            console.log(data);
+            // Lakukan tindakan setelah peminjaman berhasil disimpan
+          })
+          .catch(error => {
+            console.error(error);
+            // Lakukan tindakan jika terjadi kesalahan saat melakukan peminjaman
+          });
+    },
+    submitPengembalian() {
+      fetch('http://localhost/crud/pengembalianbuku.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(this.pengembalian),
+      })
+          .then(response => response.json())
+          .then(data => {
+            console.log(data);
+            // Lakukan tindakan setelah pengembalian berhasil disimpan
+          })
+          .catch(error => {
+            console.error(error);
+            // Lakukan tindakan jika terjadi kesalahan saat melakukan pengembalian
+          });
+    },
+    selectPeminjaman() {
+      try {
+        const response = fetch('http://localhost/crud/selectpeminjaman.php?nomor=${this.selectedNumber}');
+        if (response.ok) {
+          const data = response.json();
+          console.log(data);
+        } else {
+          console.error("Gagal memuat data idPeminjaman");
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    },
     fetchPeminjamanDipinjam() {
       // Panggil API backend PHP untuk mendapatkan data peminjaman dengan status DIPINJAM
-      // Gantikan BASE_URL dengan URL API Anda
       axios.get('http://localhost/crud/statusdipinjam.php')
           .then((response) => {
             this.peminjamanDipinjam = response.data;
@@ -82,7 +181,6 @@ export default {
     },
     fetchPeminjamanDikembalikan() {
       // Panggil API backend PHP untuk mendapatkan data peminjaman dengan status DIKEMBALIKAN
-      // Gantikan BASE_URL dengan URL API Anda
       axios.get( 'http://localhost/crud/statusdikembalikan.php')
           .then((response) => {
             this.peminjamanDikembalikan = response.data;
